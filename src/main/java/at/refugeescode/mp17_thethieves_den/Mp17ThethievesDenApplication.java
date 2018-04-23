bdfe813cb2974c3d4c36e29ccfd12ad23ce7a92d
Hadi <hadi.effatian@gmail.com>
2018-04-23 15:00:39 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp17_thethieves_den/Mp17ThethievesDenApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp17_thethieves_den;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp17ThethievesDenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp17ThethievesDenApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp17_thethieves_den/Mp17ThethievesDenApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp17_thethieves_den;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp17ThethievesDenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
